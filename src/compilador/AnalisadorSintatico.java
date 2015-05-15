57aedf0f10547b6e3f6aa8a09e7f4378b54c0756
NilMouraa <nil2678@hotmail.com>
2015-05-15 17:41:55 -0300

    AnalisadorSintatico

---
+++ src/compilador/AnalisadorSintatico.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+/**
+ *
+ * @author Nil
+ */
+public class AnalisadorSintatico {
+
+    LinkedHashMap<Integer, ArrayList<Token>> tokens;
+
+    public AnalisadorSintatico(LinkedHashMap<Integer, ArrayList<Token>> resulLexico) {
+        this.tokens = resulLexico;
+    }
+
+    void Analisar() {
+        
+    }
+    
+    void comandos(){
+        
+    }
+    void condicao(){
+        
+    }
+    void expressao(){
+        
+    }
+    void expressaoprec(){
+        
+    }
+    void termo(){
+        
+    }
+    void vetor(){
+        
+    }
+    void decimal(){
+        
+    }
+    void params(){
+        
+    }
+    void declvetor(){
+        
+    }
+    
+
+}
